bce0ffa8b08ac425d420f5ee8b2720292e68f927
JbDruet <jb.druet@codeur.online>
2021-03-24 16:55:10 +0100

    navbarplusfirstraw

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Intégration d'une maquette conçue par un webdesigner professionnel téléchargée sur Figma.">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="EcoCabins-logo.png" type="image/png">
+    <title>Ecocabins</title>
+</head>
+<body>
+    <!-- début de la nav -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="EcoCabins-white.png" id="logo" alt="le logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <span class="me-auto"></span> <!-- pour le positionnement à droite des liens -->
+      <span class="navbar-text">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item me-5">
+          <a class="nav-link active" aria-current="page" href="#">Modellen</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-link" href="#">Kopen</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-link" href="#">Hure</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-link" href="#">Nieuws</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      </span>
+    </div>
+  </div>
+</nav>
+<!-- fin de la nav -->
+</body>
--- index.php
+++ index.php
@@ -1,3 +1,48 @@
 <?php include 'header.php'; ?>
 
+<div class="container-fluid">
+<!-- début de la nav -->
+<body>
+
+<!-- fin de la nav -->
+<div class="row">
+    <img src="row1.png" id="backgroundrow1" alt="imagedefond">
+</div>
+<div class="rowtitle">
+<p>Duurzame materialen <br>
+</div>
+<div class="rowbloc">
+Alle EcoCabins hebben een hoge isolatiewaarde, <br> worden geproduceerd met indien mogelijk duurzame<br> en/of natuurlijke materialen en hebben als <br>opvallende uiterlijke kenmerk de ‘ronde hoeken’,<br> mogelijk gemaakt door het specifieke <br>materiaalgebruik.</p>
+</div>
+</body>
+<!-- row de fin de page -->
+<div class="row">
+    <div class="col-sm-12 col-md-6">
+      Jean-Baptiste
+    </div>
+    <div class="col-sm-12 col-md-6">
+      <div class="row">
+      <div class="col-sm-12 col-md-6">
+      Chloé
+    </div>
+    <div class="col-sm-12 col-md-6">
+      Hervé
+    </div>
+    <div class="col-sm-12 col-md-6">
+      Nathan
+    </div>
+    <div class="col-sm-12 col-md-6">
+      Williams
+    </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
+<!-- row de fin de page -->
+
+
+
+
+
 <?php include 'footer.php'; ?>
